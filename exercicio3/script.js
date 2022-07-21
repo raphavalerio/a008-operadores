5243b3847f04859acfdae6831268e96489274fa0
Raphael Valerio <raphaelsvalerio@hotmail.com>
2022-07-20 21:52:30 -0300

    Exercícios para casa - A008

---
+++ exercicio3/script.js
@@ -0,0 +1,15 @@
+//a
+console.log("5 é maior que 20 e também é menor que 2?",
+5 > 20 && 5 < 2)
+
+//b
+console.log("5 é igual a 5 ou é igual a “5”",
+5 === 5 || 5 === "5")
+
+//c
+console.log("negação de vinte é maior que cinquenta",
+!20 > 50)
+
+//d
+console.log("negação de (vinte é maior que cinquenta)",
+!(20 > 50))
